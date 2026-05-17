80ab3d9979818e3e55eb6b48513d315341278f9a
Gunter05 <bitoukoaousmane@gmail.com>
2026-05-18 00:54:00 +0100

    fix(display): affichage et correction des utilitaires

--- utils.h
+++ utils.h
@@ -1,13 +1,10 @@
-#ifdef UTILS_H
+#ifndef UTILS_H
 #define UTILS_H
 
+#include <sys/types.h>
 #include <sys/stat.h>
 
-void format_permissions(mode_tmode, char *str);
-void format_size_readable(off_tsize, char *str);
+void format_permissions(mode_t mode, char *str);
+void format_size_readable(off_t size, char *str);
 
 #endif
-
-
-
-
